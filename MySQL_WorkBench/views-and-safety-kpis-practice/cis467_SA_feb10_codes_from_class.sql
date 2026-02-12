a9356b3a87fdf71a70e22431bdf37ec95ec28f8a
Karina Hsu <karina.hsu2000@gmail.com>
2026-02-12 09:08:30 -0500

    Add files via upload

---
+++ MySQL_WorkBench/views-and-safety-kpis-practice/cis467_SA_feb10_codes_from_class.sql
@@ -0,0 +1,77 @@
+-- views are query stored rather than tables
+SELECT * FROM
+invoices;
+-- show only first 3 columns in a view
+CREATE VIEW invoice_3_columns AS 
+SELECT invoice_id,vendor_id,invoice_number
+ FROM invoices;
+
+SELECT * FROM invoice_3_columns;
+
+-- views allow partial access to DB and gives most
+-- up to date results
+
+-- store query as a Table
+-- show only first 3 columns in a view
+USE ap;
+CREATE TABLE invoice_3_columns_table AS 
+SELECT invoice_id,vendor_id,invoice_number
+ FROM invoices;
+ SELECT * FROM invoice_3_columns_table; 
+-- running query on a stored table will not provide
+-- up to date results but will be faster than query on view 
+
+-- to make a view updatable it should be a very simple query in view 
+
+-- is this view updatable? YES
+
+CREATE VIEW invoice_3_columns AS 
+SELECT invoice_id,vendor_id,invoice_number
+ FROM invoices; 
+ 
+ -- is this view updatable? NO
+
+CREATE VIEW invoice_3_columns AS 
+SELECT invoice_id,COUNT(vendor_id),invoice_number
+ FROM invoices; 
+ 
+ USE safety;
+
+select * FROM locations;
+-- Data Warehouse - kpis view
+CREATE OR REPLACE VIEW safetykpis AS
+SELECT locations.Location_ID, Headcount, 
+		IF(Employee_Safety_Committee = "yes", 1, 0) AS committee,
+		NMI, LTI, Last_Training, Last_Audit
+FROM locations 
+	JOIN 	(	SELECT 	Location_ID, COUNT(nmi.Incident_ID) AS NMI
+				FROM nmi 
+				GROUP BY Location_ID
+			) AS NMI_query
+		ON locations.Location_ID = NMI_query.Location_ID
+	JOIN 	(	SELECT 	Location_ID, COUNT(lti.Incident_ID) AS LTI
+				FROM lti 
+				GROUP BY Location_ID
+			) AS LTI_query
+		ON locations.Location_ID = LTI_query.Location_ID
+	JOIN 	(	SELECT 	Location_ID, 
+						MAX(STR_TO_DATE(Training_Date, "%m/%d/%Y")) AS Last_Training
+				FROM trainings
+				GROUP BY Location_ID
+			) AS last_training
+		ON locations.Location_ID = last_training.Location_ID
+	JOIN 	(	SELECT 	Location_ID, 
+						MAX(STR_TO_DATE(Audit_Date, "%m/%d/%Y")) AS Last_Audit
+				FROM audits
+				GROUP BY Location_ID
+			) AS last_audit
+		ON locations.Location_ID = last_audit.Location_ID;
+        
+ -- open Data Warehouse (DW) and run queries on DW not on original database 
+SELECT * FROM safetykpis;
+
+-- calculate proportion of incidents by location on DW
+-- show top 5 with largest nmi incidents/lti 
+SELECT location_ID, NMI/Headcount AS proportion_of_NMI,
+LTI/Headcount AS proportion_of_LTI
+FROM safetykpis ORDER BY NMI/Headcount DESC LIMIT 5;
